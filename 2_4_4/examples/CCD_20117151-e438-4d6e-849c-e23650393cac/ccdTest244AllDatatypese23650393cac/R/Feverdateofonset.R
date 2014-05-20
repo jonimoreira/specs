c7a0150aa5eb186ca6ea5919b065e323d88586e4
Tim Cook <tim@mlhim.org>
2014-05-20 18:46:09 -0300

    Added complete example for 2.4.4 from the CCD-Gen.

---
+++ 2_4_4/examples/CCD_20117151-e438-4d6e-849c-e23650393cac/ccdTest244AllDatatypese23650393cac/R/Feverdateofonset.R
@@ -0,0 +1,189 @@
+# Copyright 2014, Timothy W. Cook <tim@mlhim.org>
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#' Convert XML instance subset to a dataframe.
+#'
+#' Returns a data.frame of the collected nodes of: \code{Fever: date of onset} from the XML instance passed as fileName.
+#' The XML element name is el-29d84f3a-a351-4328-9252-5d451997cf80 as a restriction of the mlhim244:DvTemporal
+#' The vectors are: ccd, data_name, ev-name, valid_time_begin, valid_time_end, ev_name, fileName,
+#' reference_ranges, normal_status
+#' One or more of: dvtemporal_date, dvtemporal_time, dvtemporal_datetime, dvtemporal_day, dvtemporal_month, dvtemporal_year, dvtemporal_year_month, dvtemporal_month_day, dvtemporal_duration, dvtemporal_ymduration, dvtemporal_dtduration
+#' 
+#' Date of onset of the symptom fever. It gives the precise lenght of the fever, which is important, since onlu subjects with 3 weeks of fever or less are eligible for this protocol.
+#' 
+#' @references
+#' The data is structured according to the Multi-Level Healthcare Information Modelling (MLHIM) Reference Model (RM) 2.4.4
+#' \url{http://www.mlhim.org}
+#' The semantic reference(s) for this data:
+#' @references
+#' \code{rdf:isDefinedBy} \url{http://bvsms.saude.gov.br/bvs/periodicos/boletim_eletronico_epi_ano04_n04.pdf}
+#' 
+#' @param fileList - The path/file name(s) of the XML file(s) to process.
+#' @return A dataframe consisting of the vectors listed in the Description.
+#' 
+#' @examples
+#' files <- dir('./inst/examples', recursive=TRUE, full.names=TRUE, pattern='\\.xml$')
+#' Feverdateofonset <- getFeverdateofonset(files) 
+#' 
+#' @export
+getFeverdateofonset <- function(fileList)
+{
+    ldata <- lapply(fileList, parseFeverdateofonset)
+    clean <- ldata[!is.na(ldata)]
+    data <- do.call(rbind,clean)
+    return(data)
+}
+
+parseFeverdateofonset <- function(fileName)
+{
+  doc <- xmlTreeParse(fileName, handlers=list('comment'=function(x,...){NULL}), asTree = TRUE)
+  root <- xmlRoot(doc)
+  ccduri <- ccduri() # function in each CCD metadata file
+  nsDEF <- nsDEF() # function in each CCD metadata file
+  pct <- getNodeSet(root, '//ccd:el-29d84f3a-a351-4328-9252-5d451997cf80', nsDEF)
+  # assign common vectors a default
+  data_name <- "Invalid data -- Missing data-name."
+  ev_name <- NA
+  valid_time_begin <- NA
+  valid_time_end <- NA
+  if (length(pct) > 0)
+  {
+      ccd <- xmlName(root)
+      # test for an ExceptionalValue
+      children <- getNodeSet(root, '//ccd:el-29d84f3a-a351-4328-9252-5d451997cf80/child::node()', nsDEF)
+      if (!is.na(charmatch('mlhim244:',children[[2]]))) # the exceptional value elements are in the RM namespace
+      {
+        n <- getNodeSet(root, paste('//ccd:el-29d84f3a-a351-4328-9252-5d451997cf80/',xmlName(children[[2]]),'/ev-name'), nsDEF)
+        ev_name <- xmlValue(n[[1]])
+      }
+      
+      n <- getNodeSet(root, '//ccd:el-29d84f3a-a351-4328-9252-5d451997cf80/data-name', nsDEF)
+      if (length(n) > 0)
+      {
+        data_name <- xmlValue(n[[1]])
+      }
+      
+      n <- getNodeSet(root, '//ccd:el-29d84f3a-a351-4328-9252-5d451997cf80/valid-time-begin', nsDEF)
+      if (length(n) > 0)
+      {
+        ds <- xmlValue(n[[1]])
+        ds <- gsub('T', ' ', ds)
+        valid_time_begin <- as.POSIXct(ds)
+      }
+      
+      n <- getNodeSet(root, '//ccd:el-29d84f3a-a351-4328-9252-5d451997cf80/valid-time-end', nsDEF)
+      if (length(n) > 0)
+      {
+        ds <- xmlValue(n[[1]])
+        ds <- gsub('T', ' ', ds)
+        valid_time_end <- as.POSIXct(ds)
+      }
+  
+      # PcT defaults
+      reference_ranges <- NA
+      normal_status <- NA
+      dvtemporal_date <- NA
+      dvtemporal_time <- NA
+      dvtemporal_datetime <- NA
+      dvtemporal_day <- NA
+      dvtemporal_month <- NA
+      dvtemporal_year <- NA
+      dvtemporal_year_month <- NA
+      dvtemporal_month_day <- NA
+      dvtemporal_duration <- NA
+      dvtemporal_ymduration <- NA
+      dvtemporal_dtduration <- NA
+      
+      n <- getNodeSet(root, '//ccd:el-29d84f3a-a351-4328-9252-5d451997cf80/normal-status', nsDEF)
+      if (length(n) > 0)
+      {
+        normal_status <- xmlValue(n[[1]])
+      }
+      
+      n <- getNodeSet(root, '//ccd:el-29d84f3a-a351-4328-9252-5d451997cf80/dvtemporal-date', nsDEF)
+      if (length(n) > 0)
+      {
+        dvtemporal_date <- xmlValue(n[[1]])
+      }
+      
+      n <- getNodeSet(root, '//ccd:el-29d84f3a-a351-4328-9252-5d451997cf80/dvtemporal-time', nsDEF)
+      if (length(n) > 0)
+      {
+        dvtemporal_time <- xmlValue(n[[1]])
+      }
+      
+      n <- getNodeSet(root, '//ccd:el-29d84f3a-a351-4328-9252-5d451997cf80/dvtemporal-datetime', nsDEF)
+      if (length(n) > 0)
+      {
+        dvtemporal_datetime <- xmlValue(n[[1]])
+      }
+      
+      n <- getNodeSet(root, '//ccd:el-29d84f3a-a351-4328-9252-5d451997cf80/dvtemporal-day', nsDEF)
+      if (length(n) > 0)
+      {
+        dvtemporal_day <- xmlValue(n[[1]])
+      }
+      
+      n <- getNodeSet(root, '//ccd:el-29d84f3a-a351-4328-9252-5d451997cf80/dvtemporal-month', nsDEF)
+      if (length(n) > 0)
+      {
+        dvtemporal_month <- xmlValue(n[[1]])
+      }
+      
+      n <- getNodeSet(root, '//ccd:el-29d84f3a-a351-4328-9252-5d451997cf80/dvtemporal-year', nsDEF)
+      if (length(n) > 0)
+      {
+        dvtemporal_year <- xmlValue(n[[1]])
+      }
+      
+      n <- getNodeSet(root, '//ccd:el-29d84f3a-a351-4328-9252-5d451997cf80/dvtemporal-year-month', nsDEF)
+      if (length(n) > 0)
+      {
+        dvtemporal_year_month <- xmlValue(n[[1]])
+      }
+      
+      n <- getNodeSet(root, '//ccd:el-29d84f3a-a351-4328-9252-5d451997cf80/dvtemporal-month-day', nsDEF)
+      if (length(n) > 0)
+      {
+        dvtemporal_month_day <- xmlValue(n[[1]])
+      }
+      
+      n <- getNodeSet(root, '//ccd:el-29d84f3a-a351-4328-9252-5d451997cf80/dvtemporal-duration', nsDEF)
+      if (length(n) > 0)
+      {
+        dvtemporal_duration <- xmlValue(n[[1]])
+      }
+      
+      n <- getNodeSet(root, '//ccd:el-29d84f3a-a351-4328-9252-5d451997cf80/dvtemporal-ymduration', nsDEF)
+      if (length(n) > 0)
+      {
+        dvtemporal_ymduration <- xmlValue(n[[1]])
+      }
+      
+      n <- getNodeSet(root, '//ccd:el-29d84f3a-a351-4328-9252-5d451997cf80/dvtemporal-dtduration', nsDEF)
+      if (length(n) > 0)
+      {
+        dvtemporal_dtduration <- xmlValue(n[[1]])
+      }
+      
+      data <- data.frame(ccd, data_name, valid_time_begin, valid_time_end,
+                         normal_status, reference_ranges, dvtemporal_date, dvtemporal_time, dvtemporal_datetime, dvtemporal_day, dvtemporal_month, dvtemporal_year, dvtemporal_year_month, dvtemporal_month_day, dvtemporal_duration, dvtemporal_ymduration, dvtemporal_dtduration,
+                         ev_name, fileName, stringsAsFactors = FALSE)
+      
+  
+  } else
+  {
+      data <- NA
+  }
+  
+  return(data)
+  
+}
---
+++ 2_4_4/examples/CCD_20117151-e438-4d6e-849c-e23650393cac/ccdTest244AllDatatypese23650393cac/R/TestDvMediafor244.R
@@ -0,0 +1,172 @@
+# Copyright 2014, Timothy W. Cook <tim@mlhim.org>
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#' Convert XML instance subset to a dataframe.
+#'
+#' Returns a data.frame of the collected nodes of: \code{Test DvMedia for 2.4.4} from the XML instance passed as fileName.
+#' The XML element name is el-67c9a5a1-a869-4b9d-a677-7a86b38788a1 as a restriction of the mlhim244:DvMedia
+#' The vectors are: ccd, data_name, ev-name, valid_time_begin, valid_time_end, ev_name, fileName,
+#' size, encoding, language, mime_type, compression_type, hash_result, hash_function, alt_txt, uri, media_content
+#' 
+#' Test DvMedia for 2.4.4
+#' 
+#' @references
+#' The data is structured according to the Multi-Level Healthcare Information Modelling (MLHIM) Reference Model (RM) 2.4.4
+#' \url{http://www.mlhim.org}
+#' The semantic reference(s) for this data:
+#' (none provided by the modeller)
+#' 
+#' @param fileList - The path/file name(s) of the XML file(s) to process.
+#' @return A dataframe consisting of the vectors listed in the Description.
+#' 
+#' @examples
+#' files <- dir('./inst/examples', recursive=TRUE, full.names=TRUE, pattern='\\.xml$')
+#' TestDvMediafor244 <- getTestDvMediafor244(files) 
+#' 
+#' @export
+getTestDvMediafor244 <- function(fileList)
+{
+    ldata <- lapply(fileList, parseTestDvMediafor244)
+    clean <- ldata[!is.na(ldata)]
+    data <- do.call(rbind,clean)
+    return(data)
+}
+
+parseTestDvMediafor244 <- function(fileName)
+{
+  doc <- xmlTreeParse(fileName, handlers=list('comment'=function(x,...){NULL}), asTree = TRUE)
+  root <- xmlRoot(doc)
+  ccduri <- ccduri() # function in each CCD metadata file
+  nsDEF <- nsDEF() # function in each CCD metadata file
+  pct <- getNodeSet(root, '//ccd:el-67c9a5a1-a869-4b9d-a677-7a86b38788a1', nsDEF)
+  # assign common vectors a default
+  data_name <- "Invalid data -- Missing data-name."
+  ev_name <- NA
+  valid_time_begin <- NA
+  valid_time_end <- NA
+  if (length(pct) > 0)
+  {
+      ccd <- xmlName(root)
+      # test for an ExceptionalValue
+      children <- getNodeSet(root, '//ccd:el-67c9a5a1-a869-4b9d-a677-7a86b38788a1/child::node()', nsDEF)
+      if (!is.na(charmatch('mlhim244:',children[[2]]))) # the exceptional value elements are in the RM namespace
+      {
+        n <- getNodeSet(root, paste('//ccd:el-67c9a5a1-a869-4b9d-a677-7a86b38788a1/',xmlName(children[[2]]),'/ev-name'), nsDEF)
+        ev_name <- xmlValue(n[[1]])
+      }
+      
+      n <- getNodeSet(root, '//ccd:el-67c9a5a1-a869-4b9d-a677-7a86b38788a1/data-name', nsDEF)
+      if (length(n) > 0)
+      {
+        data_name <- xmlValue(n[[1]])
+      }
+      
+      n <- getNodeSet(root, '//ccd:el-67c9a5a1-a869-4b9d-a677-7a86b38788a1/valid-time-begin', nsDEF)
+      if (length(n) > 0)
+      {
+        ds <- xmlValue(n[[1]])
+        ds <- gsub('T', ' ', ds)
+        valid_time_begin <- as.POSIXct(ds)
+      }
+      
+      n <- getNodeSet(root, '//ccd:el-67c9a5a1-a869-4b9d-a677-7a86b38788a1/valid-time-end', nsDEF)
+      if (length(n) > 0)
+      {
+        ds <- xmlValue(n[[1]])
+        ds <- gsub('T', ' ', ds)
+        valid_time_end <- as.POSIXct(ds)
+      }
+  
+      # PcT defaults
+      size <- NA
+      encoding <- NA
+      language <- NA
+      mime_type <- NA
+      compression_type <- NA
+      hash_result <- NA
+      hash_function <- NA
+      alt_txt <- NA
+      uri <- NA
+      media_content <- NA
+      
+      n <- getNodeSet(root, '//ccd:el-67c9a5a1-a869-4b9d-a677-7a86b38788a1/size', nsDEF)
+      if (length(n) > 0)
+      {
+        size <- as.numeric(xmlValue(n[[1]]))
+      }
+      
+      n <- getNodeSet(root, '//ccd:el-67c9a5a1-a869-4b9d-a677-7a86b38788a1/encoding', nsDEF)
+      if (length(n) > 0)
+      {
+        encoding <- xmlValue(n[[1]])
+      }
+      
+      n <- getNodeSet(root, '//ccd:el-67c9a5a1-a869-4b9d-a677-7a86b38788a1/language', nsDEF)
+      if (length(n) > 0)
+      {
+        language <- xmlValue(n[[1]])
+      }
+      
+      n <- getNodeSet(root, '//ccd:el-67c9a5a1-a869-4b9d-a677-7a86b38788a1/mime-type', nsDEF)
+      if (length(n) > 0)
+      {
+        mime_type <- xmlValue(n[[1]])
+      }
+      
+      n <- getNodeSet(root, '//ccd:el-67c9a5a1-a869-4b9d-a677-7a86b38788a1/compression-type', nsDEF)
+      if (length(n) > 0)
+      {
+        compression_type <- xmlValue(n[[1]])
+      }
+      
+      n <- getNodeSet(root, '//ccd:el-67c9a5a1-a869-4b9d-a677-7a86b38788a1/hash-result', nsDEF)
+      if (length(n) > 0)
+      {
+        hash_result <- xmlValue(n[[1]])
+      }
+      
+      n <- getNodeSet(root, '//ccd:el-67c9a5a1-a869-4b9d-a677-7a86b38788a1/hash-function', nsDEF)
+      if (length(n) > 0)
+      {
+        hash_function <- xmlValue(n[[1]])
+      }
+      
+      n <- getNodeSet(root, '//ccd:el-67c9a5a1-a869-4b9d-a677-7a86b38788a1/alt-txt', nsDEF)
+      if (length(n) > 0)
+      {
+        alt_txt <- xmlValue(n[[1]])
+      }
+      
+      n <- getNodeSet(root, '//ccd:el-67c9a5a1-a869-4b9d-a677-7a86b38788a1/uri', nsDEF)
+      if (length(n) > 0)
+      {
+        uri <- xmlValue(n[[1]])
+      }
+      
+      n <- getNodeSet(root, '//ccd:el-67c9a5a1-a869-4b9d-a677-7a86b38788a1/media-content', nsDEF)
+      if (length(n) > 0)
+      {
+        media_content <- xmlValue(n[[1]])
+      }
+      
+      data <- data.frame(ccd, data_name, valid_time_begin, valid_time_end,
+                         media_content,mime_type,language, size, encoding, alt_txt,uri,hash_function,hash_result,compression_type,
+                         ev_name, fileName, stringsAsFactors = FALSE)
+      
+  
+  } else
+  {
+      data <- NA
+  }
+  
+  return(data)
+  
+}
