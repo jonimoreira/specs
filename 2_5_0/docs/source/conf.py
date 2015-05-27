58c715356f663def851c104b4d9828db202012e6
Tim Cook <tim@mlhim.org>
2015-05-27 10:05:20 -0300

    Changed theme to RTD.

--- 2_5_0/docs/source/conf.py
+++ 2_5_0/docs/source/conf.py
@@ -16,6 +16,8 @@
 import sys
 import os
 import shlex
+import sphinx_rtd_theme
+
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -50,7 +52,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'MLHIM'
-copyright = '2015, Timothy W. Cook'
+copyright = '2015, Timothy W. Cook & Contributors'
 author = 'Timothy W. Cook'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -111,7 +113,10 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
+
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
