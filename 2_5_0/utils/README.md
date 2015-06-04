ce1a8d3e5d1f0dffd0ff53aee8fbda0e8993138b
Tim Cook <tim@mlhim.org>
2015-06-04 18:55:41 -0300

    Added utils folder.

---
+++ 2_5_0/utils/README.md
@@ -0,0 +1,1 @@
+Utility scripts (Python, XSLT, etc.) for manipulating schemas and other artifacts.
---
+++ 2_5_0/utils/pyMOE.py
@@ -0,0 +1,6 @@
+# pyMOE.py - Python MLHIM Ontology Extractor
+# a utility to extract the ontology from the RM schema.
+# looks for a single XML Schema doc (*.xsd) in the sibling folder named 'schema'.
+# produces a file with the same name but with an extension of 'owl'.
+# the output file is an RDF document with an ontology defined based on the extracted xs:appinfo information.
+# 
