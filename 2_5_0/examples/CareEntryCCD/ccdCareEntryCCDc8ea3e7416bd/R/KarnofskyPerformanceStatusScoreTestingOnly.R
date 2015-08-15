c55eb71c52c366a0f76a4d2d9e73d1f94031e8e3
Tim Cook <tim@mlhim.org>
2015-08-15 16:35:21 -0300

    Created new examples.

---
+++ 2_5_0/examples/CareEntryCCD/ccdCareEntryCCDc8ea3e7416bd/R/KarnofskyPerformanceStatusScoreTestingOnly.R
@@ -0,0 +1,61 @@
+# Copyright 2015, Timothy W. Cook <tim@mlhim.org>
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#' \code{Karnofsky Performance Status Score  - Testing Only.}
+#'
+#' Returns a data.frame of the collected nodes of: \code{Karnofsky Performance Status Score  - Testing Only} from the XML instance passed as fileName.
+#' The source name and the CCD ID are also included in each row to assist in identifying the source of the data.
+#' The XML element name is pcs-701fbe6d-3658-4fe3-ba63-16a39cb40f86 as a restriction of the DvOrdinal
+#' The vectors are: label, vtb, vte, #' normal_status, ordinal, symbol, rr_label, rr_definition, rr_vtb, rr_vtb, rr_is_normal, interval_label, interval_lower, interval_upper, lower_included, upper_included, lower_bounded, upper_bounded, #' ccd, sourceName,
+#' 
+#' Score from the Karnofsky Performance status scale, representing the functional capabilities of a person.
+#' 
+#' @references
+#' The data is structured according to the Multi-Level Healthcare Information Modelling Reference Model release 2.5.0
+#' \url{http://www.mlhim.org}
+#' The semantic reference(s) for this data:
+#' @references
+#' \code{rdfs:isDefinedBy} \url{https://cdebrowser.nci.nih.gov/CDEBrowser/search?elementDetails=9&FirstTimer=0&PageId=ElementDetailsGroup&publicId=2003853&version=4.2} and can be accessed via the CCD. A CCD is an XML Schema with embeded RDF.
+#' 
+#' @param fileList - The path/file name(s) of the XML file(s) to process.
+#' @return A dataframe consisting of the vectors listed in the Description.
+#' 
+#' The examples use the included files. Any source that can be processed via the XML::xmlTreeParse function can be used.
+#' @examples
+#' files <- dir('./inst/examples', recursive=TRUE, full.names=TRUE, pattern='\\.xml$')
+#' KarnofskyPerformanceStatusScoreTestingOnly <- getKarnofskyPerformanceStatusScoreTestingOnly(files) 
+#' 
+#' @export
+getKarnofskyPerformanceStatusScoreTestingOnly <- function(sourceList)
+{
+    data <- lapply(sourceList, parseKarnofskyPerformanceStatusScoreTestingOnly)
+    data <- data.table::rbindlist(data, fill=TRUE)
+    return(data)
+}
+
+#' @export
+parseKarnofskyPerformanceStatusScoreTestingOnly <- function(sourceName) {
+  nsDEF <- c(mlhim2='http://www.mlhim.org/ns/mlhim2/', xsi='http://www.w3.org/2001/XMLSchema-instance')
+  doc <- XML::xmlTreeParse(sourceName, handlers=list('comment'=function(x,...){NULL}), asTree = TRUE)
+  root <- XML::xmlRoot(doc)
+  pcm <- XML::getNodeSet(root, '//mlhim2:pcs-701fbe6d-3658-4fe3-ba63-16a39cb40f86', nsDEF)
+  data <- lapply(pcm, mlhim250rm::DvOrdinal)
+  data <- data.table::rbindlist(data, fill=TRUE)
+  if (length(data) > 0) { 
+      data$ccd <- XML::xmlName(root)
+      data$pcs <- 'pcs-701fbe6d-3658-4fe3-ba63-16a39cb40f86'
+      data$ccd <- XML::xmlName(root)
+      data$sourceName <- sourceName
+ } else {
+    data <- data.frame()
+  }
+ return(data)
+}
---
+++ 2_5_0/examples/CareEntryCCD/ccdCareEntryCCDc8ea3e7416bd/R/metadata.R
@@ -0,0 +1,37 @@
+# Copyright 2013-2015, Timothy W. Cook <tim@mlhim.org>
+# metadata.R for ccd-62acb8ac-9956-4c75-82c9-c8ea3e7416bd.xsd
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#' @title getMetadata
+#'
+#' The Concept Constraint Definition (CCD) Metadata
+#' @export
+getMetadata <- data.frame(
+  dc_title='CareEntry CCD',
+  dc_creator='Tim Cook',
+  dc_contributors='None',
+  dc_subject='CareEntry CCD  - Testing Only',
+  dc_source='http://www.mlhim.org',
+  dc_relation='None',
+  dc_coverage='Universal',
+  dc_type='MLHIM Concept Constraint Definition (CCD)',
+  dc_identifier='ccd-62acb8ac-9956-4c75-82c9-c8ea3e7416bd',
+  dc_description='Testing CCD-Gen Implementation of the MLHIM RM functionality.',
+  dc_publisher='MLHIM LAB, UERJ',
+  dc_date=as.POSIXct(strptime('2015-08-15 14:5555', '%Y-%m-%d %HH:%MM%SS')),
+  dc_format='text/xml',
+  dc_language='en-us',
+  stringsAsFactors=FALSE)
+  
+ccduri <- function(){
+    return('http://www.ccdgen.com/ccdlib/ccd-62acb8ac-9956-4c75-82c9-c8ea3e7416bd.xsd')
+}
+
