68dcabfa294190c607a875eaabfd89c9b8e55e9b
Tim Cook <tim@mlhim.org>
2015-08-18 08:43:49 -0300

    Move mlhim2.rdf Refactor mlhim2_jsonld

--- 2_5_0/utils/mlhim2_jsonld.py
+++ 2_5_0/utils/mlhim2_jsonld.py
@@ -1,10 +1,11 @@
 """
 Used to generate the JSON-LD for mlihim2 based on the canonical mlhim2.rdf
 """
+
 from rdflib import Graph, plugin
 from rdflib.serializer import Serializer
 
-g = Graph().parse('../schema/mlhim2.rdf', format='xml')
-context = {"@mlhim2": "http://www.mlhim.org/ns/mlhim2/mlhim2.jsonld", "@language": "en"}
-g.serialize('../schema/mlhim2.jsonld', format='json-ld', context=context, indent=4)
+g = Graph().parse('../RDF/mlhim2.rdf', format='xml')
+context = {"mlhim2": "http://www.mlhim.org/ns/mlhim2/"}
+g.serialize('jsonld/mlhim2.jsonld', format='json-ld', context=context, indent=4)
 
--- 2_5_0/utils/rm2ld.py
+++ 2_5_0/utils/rm2ld.py
@@ -30,7 +30,7 @@ def getfiles(path):
             yield file
 
 
-def rmmain(path):
+def rmmain():
     rootdir = '.'
     nsDict = {'xs': 'http://www.w3.org/2001/XMLSchema',
               'rdf': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
